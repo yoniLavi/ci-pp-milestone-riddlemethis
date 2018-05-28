d1a14be60d69617795bffc77701e300642ecc48c
Chris Zielinski <ckz8780@gmail.com>
2018-05-28 03:25:36 +0000

    added ready page and routing

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
+app.secret_key = 'This shoudl be a secret!'
 
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/ready/', methods=['GET', 'POST'])
+def ready():
+    if request.method == 'POST':
+        form = request.form
+        user = form['username']
+        return render_template("riddle.html", username=user)
+    flash('You can\'t access that page directly. Enter your username below:')
+    return redirect('/')
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
