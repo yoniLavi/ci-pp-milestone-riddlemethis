619792c0112dec7590d12c4475710799b8c18e8d
Chris Zielinski <ckz8780@gmail.com>
2018-05-28 07:30:30 +0000

    Added game loop w/o attempt limit

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import os
+import json
 from flask import Flask, render_template, request, flash, redirect
 
 app = Flask(__name__)
 app.secret_key = 'This shoudl be a secret!'
 app.url_map.strict_slashes = False
+
+def get_riddle(index):
+    with open('data/riddles.json') as json_riddles:
+        riddles = json.loads(json_riddles.read())
+        return [riddle for riddle in riddles if riddle['index'] == index]
+        
+        
 
 @app.route('/')
 def index():
@@ -20,16 +28,60 @@ def ready():
     
 @app.route('/riddleme/<username>', methods=['GET', 'POST'])
 def riddleme(username):
+    score = 0
     if request.method == 'POST':
-        riddle_index = int(request.form.get('riddle_index', 0))
-        while riddle_index < 10:
-            riddle_index += 1
+        form = request.form
+        if form.get('first-question') == 'true':
+            riddle = get_riddle(1)[0]
             context = {
-                'username': username,
-                'riddle_index': riddle_index
+                'riddle_index': 1,
+                'riddle': riddle['riddle_text'],
+                'answer': riddle['riddle_answer'],
+                'username': username
             }
             return render_template('riddle.html', context=context)
-        return redirect('/')
+        else:
+            riddle_index = int(request.form.get('riddle_index'))
+            riddle = get_riddle(riddle_index)[0]
+            submitted_answer = request.form.get('submitted_answer').strip().lower()
+            actual_answer = riddle['riddle_answer'].strip().lower()
+            correct = submitted_answer == actual_answer
+            
+            print('Riddle Index: {}'.format(riddle_index))
+            print('Riddle Object: {}'.format(riddle))
+            print('Answers: \n\tSubmitted: {} --> Actual: {}'.format(submitted_answer, actual_answer))
+            print('Correct: {}'.format(correct))
+            
+            if riddle_index == 1:
+                if correct:
+                    riddle_index += 1
+                    score += 1
+                    next_riddle = get_riddle(riddle_index)[0]
+                else:
+                    next_riddle = get_riddle(riddle_index)[0]
+                    
+            elif riddle_index <= 10:
+                if correct:
+                    riddle_index += 1
+                    score += 1
+                    if riddle_index > 10:
+                        return redirect('/')
+                    else:
+                        next_riddle = get_riddle(riddle_index)[0]
+                else:
+                    if riddle_index > 10:
+                        return redirect('/')
+                    else:
+                        next_riddle = get_riddle(riddle_index)[0]
+                
+            context = {
+                'riddle_index': next_riddle['index'],
+                'riddle': next_riddle['riddle_text'],
+                'answer': next_riddle['riddle_answer'],
+                'username': username
+            }
+            return render_template('riddle.html', context=context)
+                
     flash('You can\'t access that page directly. Enter your username below:')
     return redirect('/')
         
