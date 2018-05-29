e3c997bc6279ec8b10e95d16fba078c863d79bb7
Chris Zielinski <ckz8780@gmail.com>
2018-05-29 15:51:09 +0000

    Refactored and reduced code

--- run.py
+++ run.py
@@ -1,23 +1,29 @@
 import os
 import json
 import operator
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session
 
 app = Flask(__name__)
 app.secret_key = 'This should be a secret!'
-app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False # make URLs trailing-slash-agnostic 
 
 # Get the info for the next riddle
 def get_riddle(index):
     with open('data/riddles.json') as json_riddles:
         riddles = json.loads(json_riddles.read())
-        return [riddle for riddle in riddles if riddle['index'] == index]
+        # We try to get the riddle, but if there's an index error it means we are trying to get
+        # riddles['index'] == 11 which doesn't exist. In this case we return None. Catching the error here
+        # prevents us from having a bunch of if statements in our game logic.
+        try:
+            return [riddle for riddle in riddles if riddle['index'] == index][0]
+        except IndexError:
+            return None
 
 # Inialize the game with some default values
 def init_game(username):
     score = 0
     attempt = 1
-    riddle = get_riddle(1)[0]
+    riddle = get_riddle(1)
     context = {
         'riddle_index': 1,
         'riddle': riddle['riddle_text'],
@@ -31,7 +37,6 @@ def init_game(username):
 # Function to add a player to the leaderboard after the game has been completed.
 def add_to_leaderboard(username, final_score):
     leaders = get_leaders()
-    print(leaders)
     with open('data/leaders.txt', 'a') as leaderboard:
         if not (username, final_score) in leaders:
             leaderboard.write('\n{}:{}'.format(str(username), str(final_score)))
@@ -44,6 +49,8 @@ def get_leaders():
         for leader in leaders:
             tupe = (leader.split(':')[0].strip(), int(leader.split(':')[1].strip()))
             sorted_leaders.append(tupe)
+            
+        # Sort leaders on the 2nd elem of the tuple, reverse the sort, then return the top 10
         return sorted(sorted_leaders, key=lambda x: x[1])[::-1][:10]
 
 @app.route('/')
@@ -77,78 +84,51 @@ def riddleme(username):
             attempt = int(request.form.get('attempt'))
             riddle_index = int(request.form.get('riddle_index'))
             score = int(request.form.get('current_score'))
-            riddle = get_riddle(riddle_index)[0]
+            riddle = get_riddle(riddle_index)
             
             # Check whether the answer is correct
             submitted_answer = request.form.get('submitted_answer').strip().lower()
             actual_answer = riddle['riddle_answer'].strip().lower()
             correct = submitted_answer == actual_answer
             
-            ''' If we're on the first riddle, check whether or not it's correct.
-            If it's correct, add 1 to score, reset attempt to 1 for the next riddle,
-            and increment the riddle index. Then get the next riddle. 
-            
-            If it's wrong, check whether we're on attempt #2 and if so, increment the
-            riddle index w/o adding to the score, then get the next riddle. If not, just
-            return the same riddle again after incrementing the attempt. '''
-            
-            if riddle_index == 1:
+            # Scoring/game logic
+            while riddle_index <= 10:
                 if correct:
                     riddle_index += 1
                     score += 1
                     attempt = 1
-                    next_riddle = get_riddle(riddle_index)[0]
+                    next_riddle = get_riddle(riddle_index)
                     flash('Nice work! You nailed it!', 'success')
                 else:
-                    if attempt >= 2:
+                    if attempt >= 2: 
                         riddle_index += 1
                         attempt = 1
-                        next_riddle = get_riddle(riddle_index)[0]
+                        next_riddle = get_riddle(riddle_index)
                         flash('"{}" was your last attempt. The answer was "{}". How about a new riddle?'.format(submitted_answer, actual_answer), 'error')
                     else:
                         attempt += 1
-                        next_riddle = get_riddle(riddle_index)[0]
+                        next_riddle = get_riddle(riddle_index)
                         flash('"{}" wasn\'t right. You\'ve got one more try...'.format(submitted_answer), 'error')
-            else:
-                if correct:
-                    riddle_index += 1
-                    score += 1
-                    attempt = 1
-                    if riddle_index > 10:
-                        final_score = score
-                        add_to_leaderboard(username, final_score)
-                        leaders = get_leaders()
-                        return render_template('leaders.html', final_score=final_score, leaders=leaders)
-                    else:
-                        next_riddle = get_riddle(riddle_index)[0]
-                        flash('Nice work! You nailed it!', 'success')
+                
+                # Now just populate a context dictionary to use in the template, and return the template.
+                # next_riddle will be none if we're on the final question. If so we clear flashed messages
+                # and return the leaderboard instead
+                if next_riddle is not None:
+                    context = {
+                        'riddle_index': next_riddle['index'], # We use the internal riddle index for clarity here
+                        'riddle': next_riddle['riddle_text'],
+                        'answer': next_riddle['riddle_answer'],
+                        'username': username,
+                        'current_score': score,
+                        'attempt': attempt
+                    }
+                    return render_template('riddle.html', context=context)
                 else:
-                    if riddle_index > 10:
-                        final_score = score
-                        add_to_leaderboard(username, final_score)
-                        leaders = get_leaders()
-                        return render_template('leaders.html', final_score=final_score, leaders=leaders)
-                    else:
-                        if attempt >= 2: 
-                            riddle_index += 1
-                            attempt = 1
-                            next_riddle = get_riddle(riddle_index)[0]
-                            flash('"{}" was your last attempt. The answer was "{}". How about a new riddle?'.format(submitted_answer, actual_answer), 'error')
-                        else:
-                            attempt += 1
-                            next_riddle = get_riddle(riddle_index)[0]
-                            flash('"{}" wasn\'t right. You\'ve got one more try...'.format(submitted_answer), 'error')
+                    session.pop('_flashes', None) # Clear flashed messages if we're on the final question
             
-            # Now just populate a context dictionary to use in the template, and return the template
-            context = {
-                'riddle_index': next_riddle['index'], # We use the internal riddle index for clarity here
-                'riddle': next_riddle['riddle_text'],
-                'answer': next_riddle['riddle_answer'],
-                'username': username,
-                'current_score': score,
-                'attempt': attempt
-            }
-            return render_template('riddle.html', context=context)
+            # Return final score and add the player to the leaderboard
+            add_to_leaderboard(username, score)
+            return render_template('leaders.html', final_score=score, leaders=get_leaders())
             
     # Redirect to the homepage with an error if using GET
     flash('You can\'t access that page directly. Enter your username below:')
